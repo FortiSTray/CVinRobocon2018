f4a365d8a05fd8d924df74dc7854b56915bc2620
STpiamcee42 <mechanic_liang@126.com>
2018-01-23 09:55:36 +0800

    增加decoder中用于判断该message的像素点数

--- RoboconCV/src/decoder.cpp
+++ RoboconCV/src/decoder.cpp
@@ -25,9 +25,9 @@ int Decoder::decode(Mat& img)
 	//左上信息块
 	grayscale = 0;
 	pixNum = 0;
-	for (int i = MESSAGE_LU_Y; i < MESSAGE_LU_Y + MESSAGE_SIZE; i += 1)
+	for (int i = MESSAGE_LU_Y; i < MESSAGE_LU_Y + MESSAGE_SIZE; i++)
 	{
-		for (int j = MESSAGE_LU_X; j < MESSAGE_LU_X + MESSAGE_SIZE; j += 1)
+		for (int j = MESSAGE_LU_X; j < MESSAGE_LU_X + MESSAGE_SIZE; j++)
 		{
 			grayscale += preProcImage.ptr<uchar>(i)[j];
 			pixNum++;
@@ -46,9 +46,9 @@ int Decoder::decode(Mat& img)
 	//中上信息块
 	grayscale = 0;
 	pixNum = 0;
-	for (int i = MESSAGE_MU_Y; i < MESSAGE_MU_Y + MESSAGE_SIZE; i += 3)
+	for (int i = MESSAGE_MU_Y; i < MESSAGE_MU_Y + MESSAGE_SIZE; i++)
 	{
-		for (int j = MESSAGE_MU_X; j < MESSAGE_MU_X + MESSAGE_SIZE; j += 3)
+		for (int j = MESSAGE_MU_X; j < MESSAGE_MU_X + MESSAGE_SIZE; j++)
 		{
 			grayscale += preProcImage.ptr<uchar>(i)[j];
 			pixNum++;
@@ -67,9 +67,9 @@ int Decoder::decode(Mat& img)
 	//右上信息块
 	grayscale = 0;
 	pixNum = 0;
-	for (int i = MESSAGE_RU_Y; i < MESSAGE_RU_Y + MESSAGE_SIZE; i += 3)
+	for (int i = MESSAGE_RU_Y; i < MESSAGE_RU_Y + MESSAGE_SIZE; i++)
 	{
-		for (int j = MESSAGE_RU_X; j < MESSAGE_RU_X + MESSAGE_SIZE; j += 3)
+		for (int j = MESSAGE_RU_X; j < MESSAGE_RU_X + MESSAGE_SIZE; j++)
 		{
 			grayscale += preProcImage.ptr<uchar>(i)[j];
 			pixNum++;
@@ -88,9 +88,9 @@ int Decoder::decode(Mat& img)
 	//左下信息块
 	grayscale = 0;
 	pixNum = 0;
-	for (int i = MESSAGE_LD_Y; i < MESSAGE_LD_Y + MESSAGE_SIZE; i += 3)
+	for (int i = MESSAGE_LD_Y; i < MESSAGE_LD_Y + MESSAGE_SIZE; i++)
 	{
-		for (int j = MESSAGE_LD_X; j < MESSAGE_LD_X + MESSAGE_SIZE; j += 3)
+		for (int j = MESSAGE_LD_X; j < MESSAGE_LD_X + MESSAGE_SIZE; j++)
 		{
 			grayscale += preProcImage.ptr<uchar>(i)[j];
 			pixNum++;
@@ -109,9 +109,9 @@ int Decoder::decode(Mat& img)
 	//中下信息块
 	grayscale = 0;
 	pixNum = 0;
-	for (int i = MESSAGE_MD_Y; i < MESSAGE_MD_Y + MESSAGE_SIZE; i += 3)
+	for (int i = MESSAGE_MD_Y; i < MESSAGE_MD_Y + MESSAGE_SIZE; i++)
 	{
-		for (int j = MESSAGE_MD_X; j < MESSAGE_MD_X + MESSAGE_SIZE; j += 3)
+		for (int j = MESSAGE_MD_X; j < MESSAGE_MD_X + MESSAGE_SIZE; j++)
 		{
 			grayscale += preProcImage.ptr<uchar>(i)[j];
 			pixNum++;
@@ -130,9 +130,9 @@ int Decoder::decode(Mat& img)
 	//右下信息块
 	grayscale = 0;
 	pixNum = 0;
-	for (int i = MESSAGE_RD_Y; i < MESSAGE_RD_Y + MESSAGE_SIZE; i += 3)
+	for (int i = MESSAGE_RD_Y; i < MESSAGE_RD_Y + MESSAGE_SIZE; i++)
 	{
-		for (int j = MESSAGE_RD_X; j < MESSAGE_RD_X + MESSAGE_SIZE; j += 3)
+		for (int j = MESSAGE_RD_X; j < MESSAGE_RD_X + MESSAGE_SIZE; j++)
 		{
 			grayscale += preProcImage.ptr<uchar>(i)[j];
 			pixNum++;
